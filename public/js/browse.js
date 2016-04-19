5732b28b4accf20f44b09af903ca78e1fd3e89c8
Abdelrahman Hosny <abdelrahman.hosny@hotmail.com>
2016-04-19 12:29:04 -0400

    algopiper search

--- public/js/browse.js
+++ public/js/browse.js
@@ -212,14 +212,14 @@ function md5(str) {
 var search = instantsearch({
     appId: 'CPO3M6SOV0',
     apiKey: '52f439effa428d4e07de53955207887d',
-    indexName: 'dev_AlgoRun',
+    indexName: 'dev_AlgoPiper',
     urlSync: true
 });
 
 search.addWidget(
     instantsearch.widgets.searchBox({
         container: '#search-box',
-        placeholder: 'type algorithm name or keyword...'
+        placeholder: 'type pipeline name or keyword...'
     }));
 
 search.addWidget(
@@ -228,37 +228,28 @@ search.addWidget(
         templates: {
             empty: 'No results',
             item: function(data) {
-                var algo_name = data._highlightResult.algo_name.value;
-                var algo_authors = "";
-		var idx_auth=1;
-                for(i=0; i<data.algo_authors.length;i++){
-			if (idx_auth>1) { algo_authors += ", " }
-			if(data.algo_authors[i].personal_website != undefined && data.algo_authors[i].personal_website != ""){
-				algo_authors += "<a style='text-decoration:none;' href='" + data.algo_authors[i].personal_website + "' target='_blank'>"+data.algo_authors[i].name+ "</a>&nbsp&nbsp";    
-			}else{
-				algo_authors += data.algo_authors[i].name;    
-			}
-			idx_auth+=1;
-                }
-                var algo_summary = data._highlightResult.algo_summary.value;
-                var algo_keywords ='<span style="font-size:85%;">';
-		var idx_kw=1;
-                data._highlightResult.algo_keywords.forEach(function(entry){
-		    if (idx_kw>1) { algo_keywords += " - " }
-                    algo_keywords += "<i>"+entry.value + "</i>";
-		    idx_kw+=1;
+                var pipeline_name = data._highlightResult.pipeline_name.value;
+                
+                var pipeline_summary = data._highlightResult.pipeline_summary.value;
+                var pipeline_keywords ='<span style="font-size:85%;">';
+		        var idx_kw=1;
+                data._highlightResult.pipeline_keywords.forEach(function(entry){
+		              if (idx_kw>1) { 
+                          pipeline_keywords += " - " 
+                      }
+                      pipeline_keywords += "<i>"+entry.value + "</i>";
+		              idx_kw+=1;
                 });
-		algo_keywords += '</span>'
-                var algo_link = data.url;
+		        pipeline_keywords += '</span>'
+                var pipeline_json = data.pipeline_json;
                 
                 var result_card = "<div class='col-md-4 col-sm-6'> \
 	        			<div class='service-wrapper'> \
-		        			<h2 class='search'>" + algo_name + "</h2>";
+		        			<h2 class='search'>" + pipeline_name + "</h2>";
                 result_card += " \
-		        			<p style='border-bottom:2px solid #2C3D50;padding-bottom:10px;'>" + algo_summary + "</p> \
-		        			<p style='font-size:90%;'>" + algo_authors + "</p> \
-		        			<p>" + algo_keywords + "</p> \
-		        			<div style='width:82%;position:absolute;margin:0px auto;bottom:30px;'><a href='" + algo_link + "' target='_blank' class='btn' style='text-decoration:none;'>Use now</a></div> \
+		        			<p style='border-bottom:2px solid #2C3D50;'>" + pipeline_summary + "</p> \
+		        			<p>" + pipeline_keywords + "</p> \
+		        			<div style='width:82%;position:absolute;'><a href='/resources/pipelines/" + pipeline_json + "' target='_blank' class='btn' style='text-decoration:none;'>Copy pipeline JSON</a></div> \
 		        		</div> \
 	        		</div>";
                 return result_card;
