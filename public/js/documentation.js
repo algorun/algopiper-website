3e0e24a008c8200e08c760fc9aad237d13954336
Abdelrahman Hosny <abdelrahman.hosny@hotmail.com>
2016-07-20 21:03:13 -0400

    updates documentation

--- public/js/documentation.js
+++ public/js/documentation.js
@@ -2,11 +2,11 @@ $('#doc-1').click(function(){
     $('#doc-1').attr("class","doc-side-nav doc-active");
     $('#doc-2').attr("class","doc-side-nav");
     $('#doc-3').attr("class","doc-side-nav");
-    $('#doc-pdf').attr("src", "resources/packaging-software.pdf");
+    $('#doc-pdf').attr("src", "resources/user-guide.pdf");
 });
 $('#doc-3').click(function(){
     $('#doc-3').attr("class","doc-side-nav doc-active");
     $('#doc-1').attr("class","doc-side-nav");
     $('#doc-2').attr("class","doc-side-nav");
-    $('#doc-pdf').attr("src", "resources/packaging-software-examples.pdf");
+    $('#doc-pdf').attr("src", "resources/examples.pdf");
 });
